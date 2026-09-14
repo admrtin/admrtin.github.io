9b922338e755e6470f378044a763431347b94bae
Adam Martin <admrtin@proton.me>
2026-09-14 14:58:11 -0600

    update metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Adam Martin",
+	url: "https://github.com/admrtin",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Adam Martin",
+		email: "admrtin@proton.me",
+		url: "https://admrtin.github.io"
 	}
 }
